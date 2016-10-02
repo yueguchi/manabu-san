221d949d3fa76b40732465e22654c7b5f55f9f41
AS江口 <eguchi@asial.co.jp>
2016-10-02 19:45:25 +0900

    host修正

--- api/LearnApi.php
+++ api/LearnApi.php
@@ -89,7 +89,7 @@ class LearnApi extends Common {
             error_reporting(E_ALL & ~E_NOTICE);
             $url = parse_url(getenv('DATABASE_URL'));
             var_dump($url);
-            $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+            $dsn = sprintf('mysql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
             $pdo = new PDO($dsn, $url['user'], $url['pass']);
             var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
         } catch (PDOException $e) {
