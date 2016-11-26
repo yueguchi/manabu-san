d67ed8ee18830849e041c14292140753570cf2b2
AS江口 <eguchi@asial.co.jp>
2016-11-27 01:05:09 +0900

    path修正

--- test/kojin/js/moving-department.js
+++ test/kojin/js/moving-department.js
@@ -19,7 +19,7 @@ for (var i = 0; i < departments.length; i++) {
         var emp = emps[y];
         var departmenrName = DB.getDepartment(emp.department);
         if (departmenrName === departments[i].name) {
-            var tag = "<li data-emp-id='" + emp.id + "' data-department-id='" + departments[i].id + "'>" + "<img class='small-emp-img' src='/img/" + emp.id +  ".jpg'>" + emp.name_kanji + "</li>";
+            var tag = "<li data-emp-id='" + emp.id + "' data-department-id='" + departments[i].id + "'>" + "<img class='small-emp-img' src='../img/" + emp.id +  ".jpg'>" + emp.name_kanji + "</li>";
             target.find(".emps-list").append(tag);
         }
     }
