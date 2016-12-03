8d7c22bf0092fb9f5b8d25e11870d9c7c9e5eb30
AS江口 <eguchi@asial.co.jp>
2016-12-03 22:23:52 +0900

    複数条件検索

--- test/kojin/js/index.js
+++ test/kojin/js/index.js
@@ -8,7 +8,7 @@ $('input[name="q3"]').val(q3);
 
 // データベース読み込み
 var emps;
-if(q1) {
+if (q1) {
     emps = alasql('SELECT * FROM emp WHERE number LIKE ? ', ['%' + q1 + '%']);
 } else if (q2) {
     q2 = '%' + q2 + '%';
@@ -17,150 +17,141 @@ if(q1) {
     emps = alasql('SELECT * FROM emp', []);
 }
 if (q3) {
-    emps = alasql(
-        "select * from emp " +
-        "WHERE name_kanji LIKE ? " +
-        "OR name_kana LIKE ? " +
-        "OR number LIKE ? " +
-        "OR birthday LIKE ? " +
-        "OR tel LIKE ? " +
-        "OR ctct_name LIKE ? " +
-        "OR ctct_addr LIKE ? " +
-        "OR ctct_tel LIKE ? " +
-        "OR pspt_no LIKE ? " +
-        "OR pspt_date LIKE ? " +
-        "OR pspt_name LIKE ? " +
-        "OR rental LIKE ?"
-        ,
-        [
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-        ]);
-    // empを見に行く
-    eduEmps = alasql(
-        'SELECT emp.* FROM emp JOIN edu ON emp.id = edu.emp WHERE edu.school LIKE ? OR edu.major LIKE ?'
-        ,
-        [
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-        ]
-    );
-    if (emps.length === 0) {
-        emps = eduEmps;
-    }
-    // addrも見に行く
-    addrEmps = alasql(
-        'SELECT emp.* FROM emp JOIN addr ON emp.id = addr.emp WHERE addr.city LIKE ? OR addr.street LIKE ? OR addr.bldg LIKE ?'
-        ,
-        [
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-        ]
-    );
-    if (emps.length === 0) {
-        emps = addrEmps;
-    }
-    // ファミリー
-    familyEmps = alasql(
-        'SELECT emp.* FROM emp JOIN family ON emp.id = family.emp WHERE family.name_kanji LIKE ? OR family.name_kana LIKE ? OR family.relation LIKE ?'
-        ,
-        [
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-            '%' + q3 + '%',
-        ]
-    );
-    if (emps.length === 0) {
-        emps = familyEmps;
-    }
-    
-    // その他(CHOICE関連)
-    // sex
-    sexEmps = alasql(
-        'select emp.* from emp join choice ON emp.sex = choice.id where choice.text LIKE ?'
-        , ['%' + q3 + '%']
-    );
-    if (emps.length === 0) {
-        emps = sexEmps;
-    }
-    // house
-    houseEmps = alasql(
-        'select emp.* from emp join addr ON emp.id = addr.emp JOIN choice ON addr.house = choice.id where choice.text LIKE ?'
-        , ['%' + q3 + '%']
-    );
-    if (emps.length === 0) {
-        emps = houseEmps;
-    }
-    // state
-    stateEmps = alasql(
-        'select emp.* from emp join addr ON emp.id = addr.emp JOIN choice ON addr.state = choice.id where choice.text LIKE ?'
-        , ['%' + q3 + '%']
-    );
-    if (emps.length === 0) {
-        emps = stateEmps;
-    }
-    // 所属部署
-    deptEmps = alasql(
-        'select emp.* from emp join department ON emp.id = department.emp JOIN choice ON department.department = choice.id where choice.text LIKE ?'
-        , ['%' + q3 + '%']
-    );
-    if (emps.length === 0) {
-        emps = deptEmps;
-    }
-    
-    
-    // マージ
-     var ids = [];
-     $.each(emps, function(index, emp) {
-         ids.push(emp.id);
-     });
-    // edu
-    $.each(eduEmps, function(index, eduEmp) {
-        if (ids.indexOf(eduEmp.id) < 0) {
-            emps.push(eduEmp);
+    var refines = q3.split(" ");
+    $.each(refines, function(index, q3) {
+        emps = alasql(
+            "select * from emp " +
+            "WHERE name_kanji LIKE ? " +
+            "OR name_kana LIKE ? " +
+            "OR number LIKE ? " +
+            "OR birthday LIKE ? " +
+            "OR tel LIKE ? " +
+            "OR ctct_name LIKE ? " +
+            "OR ctct_addr LIKE ? " +
+            "OR ctct_tel LIKE ? " +
+            "OR pspt_no LIKE ? " +
+            "OR pspt_date LIKE ? " +
+            "OR pspt_name LIKE ? " +
+            "OR rental LIKE ?", [
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+            ]);
+        // empを見に行く
+        eduEmps = alasql(
+            'SELECT emp.* FROM emp JOIN edu ON emp.id = edu.emp WHERE edu.school LIKE ? OR edu.major LIKE ?', [
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+            ]
+        );
+        if (emps.length === 0) {
+            emps = eduEmps;
         }
-    });
-    // addr
-    $.each(addrEmps, function(index, addrEmp) {
-        if (ids.indexOf(addrEmp.id) < 0) {
-            emps.push(addrEmp);
+        // addrも見に行く
+        addrEmps = alasql(
+            'SELECT emp.* FROM emp JOIN addr ON emp.id = addr.emp WHERE addr.city LIKE ? OR addr.street LIKE ? OR addr.bldg LIKE ?', [
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+            ]
+        );
+        if (emps.length === 0) {
+            emps = addrEmps;
         }
-    });
-    // sex
-    $.each(sexEmps, function(index, sexEmp) {
-        if (ids.indexOf(sexEmp.id) < 0) {
-            emps.push(sexEmp);
+        // ファミリー
+        familyEmps = alasql(
+            'SELECT emp.* FROM emp JOIN family ON emp.id = family.emp WHERE family.name_kanji LIKE ? OR family.name_kana LIKE ? OR family.relation LIKE ?', [
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+                '%' + q3 + '%',
+            ]
+        );
+        if (emps.length === 0) {
+            emps = familyEmps;
         }
-    });
-    // house
-    $.each(houseEmps, function(index, houseEmp) {
-        if (ids.indexOf(houseEmp.id) < 0) {
-            emps.push(houseEmp);
+
+        // その他(CHOICE関連)
+        // sex
+        sexEmps = alasql(
+            'select emp.* from emp join choice ON emp.sex = choice.id where choice.text LIKE ?', ['%' + q3 + '%']
+        );
+        if (emps.length === 0) {
+            emps = sexEmps;
         }
-    });
-    // state
-    $.each(stateEmps, function(index, stateEmp) {
-        if (ids.indexOf(stateEmp.id) < 0) {
-            emps.push(stateEmp);
+        // house
+        houseEmps = alasql(
+            'select emp.* from emp join addr ON emp.id = addr.emp JOIN choice ON addr.house = choice.id where choice.text LIKE ?', ['%' + q3 + '%']
+        );
+        if (emps.length === 0) {
+            emps = houseEmps;
         }
-    });
-    // 所属部署
-    $.each(deptEmps, function(index, deptEmp) {
-        if (ids.indexOf(deptEmp.id) < 0) {
-            emps.push(deptEmp);
+        // state
+        stateEmps = alasql(
+            'select emp.* from emp join addr ON emp.id = addr.emp JOIN choice ON addr.state = choice.id where choice.text LIKE ?', ['%' + q3 + '%']
+        );
+        if (emps.length === 0) {
+            emps = stateEmps;
         }
+        // 所属部署
+        deptEmps = alasql(
+            'select emp.* from emp join department ON emp.id = department.emp JOIN choice ON department.department = choice.id where choice.text LIKE ?', ['%' + q3 + '%']
+        );
+        if (emps.length === 0) {
+            emps = deptEmps;
+        }
+
+
+        // マージ
+        var ids = [];
+        $.each(emps, function(index, emp) {
+            ids.push(emp.id);
+        });
+        // edu
+        $.each(eduEmps, function(index, eduEmp) {
+            if (ids.indexOf(eduEmp.id) < 0) {
+                emps.push(eduEmp);
+            }
+        });
+        // addr
+        $.each(addrEmps, function(index, addrEmp) {
+            if (ids.indexOf(addrEmp.id) < 0) {
+                emps.push(addrEmp);
+            }
+        });
+        // sex
+        $.each(sexEmps, function(index, sexEmp) {
+            if (ids.indexOf(sexEmp.id) < 0) {
+                emps.push(sexEmp);
+            }
+        });
+        // house
+        $.each(houseEmps, function(index, houseEmp) {
+            if (ids.indexOf(houseEmp.id) < 0) {
+                emps.push(houseEmp);
+            }
+        });
+        // state
+        $.each(stateEmps, function(index, stateEmp) {
+            if (ids.indexOf(stateEmp.id) < 0) {
+                emps.push(stateEmp);
+            }
+        });
+        // 所属部署
+        $.each(deptEmps, function(index, deptEmp) {
+            if (ids.indexOf(deptEmp.id) < 0) {
+                emps.push(deptEmp);
+            }
+        });
     });
 }
 
@@ -190,7 +181,7 @@ $("#empsDetailBtn").on("click", function(event) {
 });
 
 $("#tbody-emps").on("change", ".emps-checks", function() {
-    if ($(".emps-checks:checked").length > 0 ) {
+    if ($(".emps-checks:checked").length > 0) {
         $("#empsDetailBtn").attr("disabled", false);
     } else {
         $("#empsDetailBtn").attr("disabled", true);
