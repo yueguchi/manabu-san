6086d1b90e5f615ed7d21a8584317b0e65651a01
AS江口 <eguchi@asial.co.jp>
2017-04-24 15:09:26 +0900

    インスタAPIテスト

--- validated.php
+++ validated.php
@@ -1,24 +1,44 @@
 <?php
 
-$code = $_GET["code"];
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+    $code = $_GET["code"];
 
-/*
-curl -F 'client_id=af44a9449105494580468e3f96177d6c' -F 'client_secret=c20db343d14447b2b5e379d781ab3767' -F 'grant_type=authorization_code' -F 'redirect_uri=https://manabu-san.herokuapp.com/validated.php' -F 'code=【上記で取得したcode】’ https://api.instagram.com/oauth/access_token
-*/
+    /*
+    curl -F 'client_id=af44a9449105494580468e3f96177d6c' -F 'client_secret=c20db343d14447b2b5e379d781ab3767' -F 'grant_type=authorization_code' -F 'redirect_uri=https://manabu-san.herokuapp.com/validated.php' -F 'code=【上記で取得したcode】’ https://api.instagram.com/oauth/access_token
+    */
 
-$url = 'https://api.instagram.com/oauth/access_token';
-$data = array(
-    'client_id' => 'af44a9449105494580468e3f96177d6c',
-    'client_secret' => 'c20db343d14447b2b5e379d781ab3767',
-    'grant_type' => 'authorization_code',
-    'redirect_uri' => 'https://manabu-san.herokuapp.com/validated.php',
-    'code' => $code
-);
-$options = array('http' => array(
-    'method' => 'POST',
-    'content' => http_build_query($data),
-));
-$contents = file_get_contents($url, false, stream_context_create($options));
-$jsonRet = json_decode($contents);
-$token = $jsonRet->access_token;
-var_dump($token);
+    $url = 'https://api.instagram.com/oauth/access_token';
+    $data = array(
+        'client_id' => 'af44a9449105494580468e3f96177d6c',
+        'client_secret' => 'c20db343d14447b2b5e379d781ab3767',
+        'grant_type' => 'authorization_code',
+        'redirect_uri' => 'https://manabu-san.herokuapp.com/validated.php',
+        'code' => $code
+    );
+    $options = array('http' => array(
+        'method' => 'POST',
+        'content' => http_build_query($data),
+    ));
+    $contents = file_get_contents($url, false, stream_context_create($options));
+    $jsonRet = json_decode($contents);
+    $token = $jsonRet->access_token;
+} else if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $q = $_POST["q"];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8" />
+    <title>インスタAPI連携</title>
+</head>
+<body>
+    <section>
+        <h1>ユーザー情報取得</h1>
+        <form method="post">
+            <p><input type="text" value="<?php echo $q; ?>" name="q" placeholder="ユーザー名を記述"></p>
+        </form>
+    </section>
+</body>
+</html>
