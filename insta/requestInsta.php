61fd6f5499aee8e374c4fba7cc9e72f985c6321e
AS江口 <eguchi@asial.co.jp>
2017-04-24 14:50:27 +0900

    インスタテスト

--- insta/requestInsta.php
+++ insta/requestInsta.php
@@ -1,5 +1,5 @@
 <?php
 $url = "https://api.instagram.com/oauth/authorize/?client_id=af44a9449105494580468e3f96177d6c&redirect_uri=https://manabu-san.herokuapp.com/validated.php&response_type=code&scope=public_content"
 
-header('Location: ' . $url);
+header("Location: {$url}");
 exit;
